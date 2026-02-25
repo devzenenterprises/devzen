b4886a080fb2f7ca286d220bf1845876b492bfa6
Shivanaidu18 <artifact350@gmail.com>
2026-02-25 16:13:08 +0530

    Fix ESLint dependency conflict1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import CertifiedExcellence from "./pages/CertifiedExcellence";
 import EnterpriseGradeSecurity from "./pages/EnterpriseGradeSecurity";
@@ -28,7 +28,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/certified-excellence" element={<CertifiedExcellence />} />
@@ -49,7 +49,7 @@ const App = () => (
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
